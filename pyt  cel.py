c7e2866ac098ed7d61e4c6cf57e22f7d4118d71e
Kushagr Chaturvedi <kushagr230706@gmail.com>
2024-08-09 18:36:23 +0530

    Add files via upload

---
+++ pyt  cel.py
@@ -0,0 +1,8 @@
+# importing calendar module
+# for calendar operations
+import calendar
+
+# using calender to print calendar of year
+# prints calendar of 2021
+print("The calender of year 2021 is : ")
+print(calendar.calendar(2010, 2, 1, 6))
